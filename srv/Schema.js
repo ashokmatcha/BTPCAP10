7c95282f2e797cafed1a60f27761e1253ded98be
Ashok kumar <ashokabm92@gmail.com>
2026-02-28 02:24:19 +0000

    Day12

--- srv/Schema.js
+++ srv/Schema.js
@@ -4,9 +4,23 @@ module.exports = class ProjectService extends cds.ApplicationService { init() {
 
   const { ProjectSet, EmployeeSet, TimesheetSet } = cds.entities('ProjectService')
 
-  this.before (['CREATE', 'UPDATE'], ProjectSet, async (req) => {
+  this.before (['CREATE', 'UPDATE'], ProjectSet, async (oEvent) => {
     debugger;
-    console.log('Before CREATE/UPDATE ProjectSet', req.data)
+    // Less than Comparision means 1 variable value less than 2 nd variable value
+   if(oEvent.data.EndDate < oEvent.data.StartDate){
+      debugger;
+      oEvent.error('End Date should not be less than Start Date')
+    }
+    // greater than Comparision means 1 variable value  greater than 2 nd variable value
+   /*  if(oEvent.data.EndDate > oEvent.data.StartDate){
+      debugger;
+    }*/
+//Both are value equal or not
+    /*    if(oEvent.data.EndDate == oEvent.data.StartDate){
+      debugger;
+    }*/
+    console.log('Before CREATE/UPDATE ProjectSet', oEvent.data)
+    debugger;
   })
   
   this.after ('READ', ProjectSet, async (projectSet, req) => {
